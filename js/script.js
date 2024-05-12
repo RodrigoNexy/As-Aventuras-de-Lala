de0769534042a856d7e43e9404a0428c04e14b4b
Rodrigo Carvalho <rodrigogoulart97@gmail.com>
2024-05-12 01:48:11 -0300

    Primeira Versão

---
+++ js/script.js
@@ -0,0 +1,12 @@
+        // Função para mostrar o formulário de registro
+        function mostrarRegistro() {
+            document.getElementById('engloba-conteudo').style.display = 'none';
+            document.getElementById('registro').style.display = 'block';
+            
+        }
+
+        // Função para mostrar o formulário de login
+        function mostrarLogin() {
+            document.getElementById('engloba-conteudo').style.display = 'block';
+            document.getElementById('registro').style.display = 'none';
+        }
